f9b59df1ddf4220fd71e3f4060a46db69fa0e26e
Pham Anh Dung <anhdungpham090@gmail.com>
2019-08-09 13:27:31 +0700

    trang cart

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -35,9 +35,12 @@
 				</tbody>
 			</table>
 		</div>
-		<button class="btn btn-sm btn-danger" style="cursor: pointer">
+		<button class="btn btn-sm btn-danger my-2" style="cursor: pointer">
 			Remove All Item <i class="fas fa-trash-restore-alt"></i>
 		</button>
+		<a href="#" class="btn btn-secondary my-2">
+			Continue Shopping <i class="fas fa-shopping-cart"></i>
+		</a>
 		<div class="table-responsive row mx-0 mt-3">
 			<table class="table table-sm table-borderless col-12 col-md-5 ml-auto">
 				<tbody>
@@ -63,53 +66,99 @@
 
 		<div class="row mx-0 my-3">
 			<div class="ml-auto">
-				<div class="mb-2 mb-lg-0 d-inline-flex">
-					<button class="btn btn-block btn-secondary">Continue Shopping</button>
+				<div class="d-inline-flex mb-2">
+					<a href="#" class="btn btn-success">
+						Purchase for myself <i class="fas fa-cash-register"></i>
+					</a>
 				</div>
-				<div class="d-inline-flex">
-					<button class="btn btn-block btn-success">Checkout</button>
+				<div class="d-inline-flex mb-2">
+					<a href="#" class="btn btn-success">Purchase as a gift <i class="fas fa-gifts"></i></a>
 				</div>
 			</div>
 		</div>
-		<div class="col-md-12 mb-5">
-			<h2><i class="fas fa-user-clock"></i>Relate content</h2>
+		<div class="row my-5 px-md-3">
+			<h2><i class="fas fa-user-clock"></i> Relate content</h2>
 		</div>
-		<div class="col-md-12">
-			<div class="row">
-				@foreach($lsPopular as $post)
-					<div class="col-12" style="padding: 15px"><div class="card" style=" width: 100%">
-							<div class="row no-gutters">
-								<div class="col-12 col-md-3">
-									<a href="{{route('post.show', $post->id)}}" class="d-block" style="padding: 0;
-										background:
-										url('https://via.placeholder.com/500/55595c/FFFFFF?text=Game Thumbnail')
-										no-repeat center; background-size: cover !important; min-height: 150px;
-										height: 100%; border-radius: 0.25rem 0 0 0.25rem; width: 100%">
-									</a>
+		<div class="row my-5">
+			@foreach($lsPopular as $post)
+				<div class="col-12" style="padding: 15px"><div class="card" style=" width: 100%">
+						<div class="row no-gutters">
+							<div class="col-12 col-md-3">
+								<a href="{{route('post.show', $post->id)}}" class="d-block" style="padding: 0;
+									background:
+									url('https://via.placeholder.com/500/55595c/FFFFFF?text=Game Thumbnail')
+									no-repeat center; background-size: cover !important; min-height: 150px;
+									height: 100%; border-radius: 0.25rem 0 0 0.25rem; width: 100%">
+								</a>
+							</div>
+							<div class="col-12 col-md-9">
+								<div class="card-body pb-md-0">
+									<h5 class="card-title">{{$post->title}}</h5>
+									<p class="card-text">
+										Price: 70,80 €
+										&bull; <small> <i class="fab fa-windows"></i>
+										<i class="fab fa-xbox"></i>
+										<i class="fab fa-playstation"></i></small>
+									</p>
 								</div>
-								<div class="col-12 col-md-9">
-									<div class="card-body pb-md-0">
-										<h5 class="card-title">{{$post->title}}</h5>
-										<p class="card-text">
-											Price: 70,80 €
-											&bull; <small> <i class="fab fa-windows"></i>
-											<i class="fab fa-xbox"></i>
-											<i class="fab fa-playstation"></i></small>
-										</p>
-									</div>
-									<div class="card-body text-center text-md-right pt-0">
-										<a href="#" class="btn btn-sm btn-success">
-											Add to cart <i class="fas fa-cart-plus"></i>
-										</a>
-									</div>
+								<div class="card-body text-center text-md-right pt-0">
+									<a href="#" class="btn btn-sm btn-success">
+										Add to cart <i class="fas fa-cart-plus"></i>
+									</a>
 								</div>
 							</div>
 						</div>
 					</div>
-				@endforeach
-			</div>
-
+				</div>
+			@endforeach
 		</div>
+		<div class="row my-5 px-md-3">
+			<h2><i class="fas fa-user-clock"></i> Sale Off</h2>
+		</div>
+		<div class="row my-5">
+			@foreach($lsPopular as $post)
+				<div class="col-12" style="padding: 15px"><div class="card" style=" width: 100%">
+						<div class="row no-gutters">
+							<div class="col-12 col-md-3">
+								<a href="{{route('post.show', $post->id)}}" class="d-block" style="padding: 0;
+									background:
+									url('https://via.placeholder.com/500/55595c/FFFFFF?text=Game Thumbnail')
+									no-repeat center; background-size: cover !important; min-height: 150px;
+									height: 100%; border-radius: 0.25rem 0 0 0.25rem; width: 100%">
+								</a>
+							</div>
+							<div class="col-12 col-md-9">
+								<div class="card-body pb-md-0">
+									<h5 class="card-title">{{$post->title}}</h5>
+									<p class="card-text">
+										Price:
+										<small>
+											<span style="text-decoration: line-through;">
+												70,80 €
+											</span>
+										</small>
+										&nbsp;
+										<strong>
+											<span style="font-size: 100%" class="badge badge-success"> -20% </span>
+										</strong>
+										56,6 €
+										&bull; <small> <i class="fab fa-windows"></i>
+										<i class="fab fa-xbox"></i>
+										<i class="fab fa-playstation"></i></small>
+									</p>
+								</div>
+								<div class="card-body text-center text-md-right">
+									<a href="#" class="btn btn-sm btn-success">
+										Add to cart <i class="fas fa-cart-plus"></i>
+									</a>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			@endforeach
+		</div>
+
 	</div>
 
 
