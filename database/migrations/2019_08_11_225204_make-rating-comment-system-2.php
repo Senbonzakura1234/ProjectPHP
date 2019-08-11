9a1f2ca778d0ca7a1c9ba2981541367f2af91970
Pham Anh Dung <anhdungpham090@gmail.com>
2019-08-11 22:53:47 +0700

    make rating system

--- database/migrations/2019_08_11_225204_make-rating-comment-system-2.php
+++ database/migrations/2019_08_11_225204_make-rating-comment-system-2.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class MakeRatingCommentSystem extends Migration
+class MakeRatingCommentSystem2 extends Migration
 {
     /**
      * Run the migrations.
@@ -13,8 +13,9 @@ class MakeRatingCommentSystem extends Migration
      */
     public function up()
     {
-	    Schema::table('comments', function ($table) {
-		    $table->bigInteger('user_id');
+	    Schema::table('comments', function($table) {
+		    $table->dropColumn('name');
+		    $table->dropColumn('email');
 	    });
     }
 
@@ -25,6 +26,9 @@ class MakeRatingCommentSystem extends Migration
      */
     public function down()
     {
-	    //
+	    Schema::table('comments', function($table) {
+		    $table->integer('name');
+		    $table->integer('email');
+	    });
     }
 }
